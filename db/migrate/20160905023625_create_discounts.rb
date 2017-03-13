e47ea109d7389d54980c025d703e57782c5200ee
Leonardo merchan <leonardo.merchan.1992@gmail.com>
2017-03-13 03:40:01 +0000

    pruebas

--- db/migrate/20160905023625_create_discounts.rb
+++ db/migrate/20160905023625_create_discounts.rb
@@ -1,8 +1,8 @@
 class CreateDiscounts < ActiveRecord::Migration
   def change
     create_table :discounts do |t|
-      t.references :societies, index: true, foreign_key: true
-      t.references :categories, index: true, foreign_key: true
+      t.references :societies, index: true
+      t.references :categories, index: true
       t.integer :discount_porcentaje
 
       t.timestamps null: false
